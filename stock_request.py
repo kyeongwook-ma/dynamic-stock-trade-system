4944d9373f564c25e4a763734586a8ea73367e00
zetyx <shwarzes89@gmail.com>
2014-01-18 11:15:20 +0900

    catch exception not folder

--- stock_request.py
+++ stock_request.py
@@ -1,7 +1,9 @@
 import urllib
 import urllib2
+import os
 
 from bs4 import BeautifulSoup
+
 from task import Task, TaskController
 from stock_list import stock_list
 
@@ -18,10 +20,16 @@ def get_rt_stocks():
 
 def get_anni_csv():
     file_list = list()
+    csv_folder = './anni/'
+
+    if not os.path.exists(csv_folder):
+        os.makedirs(csv_folder)
 
     def csv_download(code):
         url = 'http://www.google.com/finance/historical?q=KRX%3A' + code + '&output=csv'
-        csv_name = './anni/' + stock + '.csv'
+
+        csv_name = csv_folder + stock + '.csv'
+
         file_list.append(csv_name)
 
         csv_file = urllib2.urlopen(url)
@@ -32,5 +40,8 @@ def get_anni_csv():
     for stock in stock_list:
         TaskController().enqueue_task(Task(csv_download, stock))
 
-    return file_list
+    if TaskController().is_task_done():
+        return file_list
 
+
+get_anni_csv()

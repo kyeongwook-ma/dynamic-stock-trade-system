865ed9f604e32acda95d6beecdfec2032156254d
eomsky49 <lastsky0409@naver.com>
2014-01-20 17:36:17 +0900

    페어 알고리즘 ver 2
    페어 알고리즘 ver 2

--- pairs_trading_algorithm.py
+++ pairs_trading_algorithm.py
@@ -6,13 +6,9 @@ import numpy as np
 import statsmodels.tsa.stattools as ts
 
 
-class Mylist(list):
-    def __mul__(self,other):
-        return [[s,o] for s in self for o in other]
-
-
-stock_list = Mylist(
-    [
+# 초기 설정
+os.chdir('C:/Users/Haneul/Desktop/Project/Betrades/anni')
+stock_list = [
     '005930', '005380', '005490', '012330',
     '000660', '035420', '005935', '015760',
     '055550', '032830', '051910', '009540',
@@ -26,47 +22,46 @@ stock_list = Mylist(
     '036460', '088350', '010130', '090430',
     '001800', '036570', '009150', '078930'
     ]
-)
+append_csv = ['.csv']
+l = locals()
 
-append_csv = Mylist(['.csv'])
 
+class Mylist(list):
+    def __mul__(self,other):
+        return [[s,o] for s in self for o in other]
+
+
+stock_list = Mylist(stock_list)
+append_csv = Mylist(append_csv)
 combined_list = stock_list * append_csv
 
 for i in range(len(combined_list)):
     combined_list[i] = ''.join(combined_list[i])
 
-l = locals()
 for i in range(0,len(combined_list)):
         l['s_%d' % i] = pd.read_csv(combined_list[i])
         l['Close_s_%d' %i] = l['s_%d' % i].Close
-        l['log_s_%d' %i] = log(l['Close_s_%d' % i])
+        l['log_s_%d' %i] = np.log(l['Close_s_%d' % i])
 
 
-#현재 이중 for 문에서 애러가 나고 있음.
-#아래 코드의 목적은 48개의 주가에 1128개의 조합이 있는데 이들을 매치시키고자 하는 작업임. 그러나 현재 애러 상태 . 
+def y_cointegration(asset1, asset2):
+    coint_result = np.cov(asset2, asset1)[0][1] / np.var(asset2)
+    return coint_result
+
+def spread(asset1, asset2):
+    spread = asset1 - coint * asset2
+    return spread
+
+def adfTest(spread):
+    ADF_p_value = ts.adfuller(spread, 1)[1]
+    return ADF_p_value
+
 for i in range(1,len(combined_list)-1):
     for j in range(i+1,len(combined_list)):        
-
-        def y_cointegration('log_s_%d' %i, 'log_s_%d' %j):
-            coint_result = np.cov('log_s_%d' %j, 'log_s_%d' %i)[0][1] / np.var('log_s_%d' %j)
-            return coint_result
-
         coint = y_cointegration('log_s_%d' %i, 'log_s_%d' %j)
         l['coint_%d' %i] = coint
-
-        def spread('log_s_%d' %i, 'log_s_%d' %j):
-            spread = 'log_s_%d' %i - coint * 'log_s_%d' %j
-            return spread
-
         spread = spread('log_s_%d' %i, 'log_s_%d' %j)
-
-
-        def adfTest(spread):
-            ADF_p_value = ts.adfuller(spread, 1)[1]
-            return ADF_p_value
-
         ADF_p_value = adfTest(spread)
-
 
         if ADF_p_value <= 0.05:
             print    "The spread is likely mean-reverting."
@@ -105,3 +100,20 @@ for i in range(1,len(combined_list)-1):
 
 
 # this code will be updated.
+
+
+
+## 안쓰는 자료 
+# 원래 조합이 48C2 = 1128개여야 하는데 아래 알고리즘은 2256개로 불필요한 계산이 들어가 있음
+
+#def permutations(items, n):
+#    if n==0: yield []
+#    else:
+#        for i in xrange(len(items)):
+#            for cc in permutations(items[:i]+items[i+1:],n-1):
+#                yield [items[i]]+cc
+#
+#b = list(permutations(range(48), 2))
+
+
+

5224b7b03383aa61099b015447a141c76c2d0290
zetyx <shwarzes89@gmail.com>
2014-01-24 00:16:55 +0900

    restruct pair class

--- pairs_trading_algorithm.py
+++ pairs_trading_algorithm.py
@@ -1,74 +1,83 @@
 import os
-import math
+
 from pandas import Series
 import pandas as pd
 import numpy as np
 import statsmodels.tsa.stattools as ts
 from stock_list import *
-import sys
 
 
 os.chdir('../anni')
-l = locals()
-count = 0
-dissatisfired_adf_test = arange(10)
-combined_list = list(map(lambda x : x + '.csv', filter(lambda x: x != '035420', stock_list)))
 
-
-# a simple class with a write method
 
 def adf_test(asset):
-    return ts.adfuller(asset, 1)[1] 
+    return ts.adfuller(asset, 1)[1]
 
-satisfied_list = list()
 
-# 우리가 쓰는게 공적분 방법인데 공적분을 쓰려면 시계열이 미리 안정적이면 안됨. 불안정한 것을 공적분해서 안정화 시키는 것이 목적이기 때문.
-# 따라서 미리 안정한(stationary) 주가 목록을 제외하고 우리가 쓸수 있는 주가 목록을 추출하고자 함. 
-for i in range(0,len(combined_list)):
-    l['a_%d' % i] = pd.read_csv(combined_list[i])
-    l['Close_a_%d' %i] = l['a_%d' % i].Close
-    if adfTest(l['Close_a_%d' % i]) >= 0.05:
-        print >> satisfied_list, combined_list[i]
-
-for i in range(0,len(satisfied_list)):
-    l['s_%d' % i] = pd.read_csv(satisfied_list[i])
-    l['Close_s_%d' %i] = l['s_%d' % i].Close
-    l['log_s_%d' %i] = np.log(l['Close_s_%d' % i])
-
-# 아래 공적분 구하는 공식은 약식임. 재대로 구하려면 복잡함에따라 머신러닝이나 시뮬레이션 기법을 활용해야함. 
 def y_cointegration(asset1, asset2):
     return np.cov(asset2, asset1)[0][1] / np.var(asset2)
 
 
-for i in range(0,len(satisfied_list)-1):
-    for j in range(i+1,len(satisfied_list)):        
-        l['coint_%d' %i] = y_cointegration(l['log_s_%d' %i], l['log_s_%d' %j])
-        spread = l['log_s_%d' %i] - l['coint_%d' %i] * l['log_s_%d' %j]
-        ADF_p_value = adfTest(spread)
-        if ADF_p_value <= 0.05:
-            print    "The spread is likely mean-reverting , p-value of ADF Test is ", ADF_p_value
-        else:
-            print    "The spread is not mean-reverting , p-value of ADF Test is ", ADF_p_value 
-        spread = Series(spread)
-        signalMean = spread.mean()
-        signalDev = spread.std()
-        openMult = 1.0
-        closeMult = 0.5
-        stopLossMult = 4.0
-        openSignal = signalDev * openMult;
-        closeSignal = signalDev * closeMult;
-        stopLossSignal = signalDev * stopLossMult;
-        residSpread = spread - signalMean
+def plot_sig(signal, spread):
+    up = signal * (spread * 0 + 1)
+    down = -signal * (spread * 0 + 1)
+    up.plot()
+    down.plot()
+    spread.plot()
 
-        def plotSig(signal):
-            up = signal * (residSpread * 0 + 1) 
-            down = -signal * (residSpread * 0 + 1)
-            up.plot()
-            down.plot() 
-            residSpread.plot()
-            
-        plotSig(openSignal)
-        plotSig(closeSignal)
-        plotSig(stopLossSignal)
+
+class Pair(object):
+    """docstring for Pair"""
+
+    def __init__(self):
+        super(Pair, self).__init__()
+        self._list = list(map(lambda x: x + '.csv', filter(lambda x: x != '035420', stock_list)))
+        self.satisfied_list = list()
+        self.spread = 0
+        self.signal_mean = 0
+        self.signal_dev = 0
+        self.open_mult = 1.0
+        self.close_mult = 0.5
+        self.stoploss_mult = 4.0
+        self.open_signal = 0
+        self.close_signal = 0
+        self.stoploss_signal = 0
+        self.resid_spread = 0
+        self.ADF_p_value = 0
+
+    def check_adf(self):
+        for i in range(0, len(self._list)):
+            origin = pd.read_csv(self._list[i]).Close
+
+            if adf_test(origin) >= 0.05:
+                loged = np.log(origin)
+                self.satisfied_list.append(loged)
+
+    def create_pair(self):
+        for i in range(0, len(self.satisfied_list) - 1):
+            for j in range(i + 1, len(self.satisfied_list)):
+
+            coint = y_cointegration(self.satisfied_list[i], self.satisfied_list[j])
+
+            self.spread = self.satisfied_list[i] - coint * self.satisfied_list[j]
+            ADF_p_value = adf_test(self.spread)
+
+            if self.ADF_p_value <= 0.05:
+                print    "The spread is likely mean-reverting , p-value of ADF Test is ", ADF_p_value
+            else:
+                print    "The spread is not mean-reverting , p-value of ADF Test is ", ADF_p_value
+
+            self.spread = Series(self.spread)
+            self.signal_mean = self.spread.mean()
+            self.signal_dev = self.spread.std()
+            self.open_mult = 1.0
+            self.close_mult = 0.5
+            self.stoploss_mult = 4.0
+            self.open_signal = self.signalDev * self.openMult
+            self.close_signal = self.signalDev * self.closeMult
+            self.stoploss_signal = self.signalDev * self.stopLossMult
+            self.resid_spread = self.spread - self.signalMean
+
+
 
 
--- rts_datamodel.py
+++ rts_datamodel.py
@@ -9,14 +9,15 @@ class StockDataModel(object):
         self.data_list = []
         self.pair_list = []
 
-    def add_data(self, stock, is_multiple=False, idx=0):
+    def add_data(self, data, is_multiple=False, idx=0):
+
 
         if is_multiple is True:
-            self.data_list.extend(stock)
+            self.data_list.extend(data)
         else:
-            self.data_list.append(stock)
+            self.data_list.append(data)
         if idx != 0:
-            self.data_list.index(stock, idx)
+            self.data_list.index(data, idx)
 
     def get_list_size(self):
         return len(self.data_list)
